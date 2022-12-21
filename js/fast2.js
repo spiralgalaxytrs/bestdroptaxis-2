d0346f0bdddccb2e1796b681256a0d0f59fd192b
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2022-12-21 21:56:20 +0530

    booking

--- js/fast2.js
+++ js/fast2.js
@@ -19,7 +19,7 @@ function testVariable() {
     else if(strText7=="Traveller")
     {rate=traveller;}
 
-    var result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate;
+    var result = 'Best Drop Taxis Booking ' + '%0ACustomer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate;
 
 var finalMsg = encodeURI(result);
          document.getElementById("bookingForm").addEventListener("submit", (e) => {
@@ -34,7 +34,7 @@ if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6
         request.open("post", url);
         request.send();
          //  replace
-            var wnd = window.open("https://www.fast2sms.com/dev/bulk?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&sender_id=FSTSMS&message=https://bestonewaytaxis.com%0AContact No: 9994451282%0AHello "+strText+",%0AThank you for Booking with Best OneWay Taxi.Your "+strText2+" Booking has been Confirmed on "+strText4+".&language=english&route=p&numbers="+strText1);
+            var wnd = window.open("https://www.fast2sms.com/dev/bulk?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&sender_id=FSTSMS&message=https://bestdroptaxis.in%0AContact No: 9994451282%0AHello "+strText+",%0AThank you for Booking with Best OneWay Taxi.Your "+strText2+" Booking has been Confirmed on "+strText4+".&language=english&route=p&numbers="+strText1);
 		//var wnd1=location.replace("bc.html","_self ");		      
                               if(wnd){
                                       setTimeout(function () { wnd.close();}, 1800);
